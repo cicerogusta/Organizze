b4983ac0305464897858eb29c6461efbf25facb0
cicer <ciceroandrade2525@gmail.com>
2024-01-22 12:39:23 -0300

    corrigindo exibição das movimentacoes

--- app/src/main/java/com/cicerodev/yourmoney/adapter/MovimentacaoAdapter.kt
+++ app/src/main/java/com/cicerodev/yourmoney/adapter/MovimentacaoAdapter.kt
@@ -31,48 +31,51 @@ class MovimentacaoAdapter(
         if (movimentacao.tipo == "d" ) {
             holder.binding.textValor.setTextColor(context.resources.getColor(R.color.colorAccentDespesa))
             holder.binding.textValor.text = "-" + movimentacao.valor.toString()
-
             if (movimentacao.isDespesaCartao) {
                 holder.binding.txtCardName.visibility = View.VISIBLE
                 holder.binding.txtCardName.text = movimentacao.cartaoCredito.nomeCartao
-                holder.binding.imageMoney.visibility = View.GONE
                 holder.binding.imagePix.visibility = View.GONE
+                holder.binding.imageMoney.visibility = View.GONE
+
             } else {
-                holder.binding.txtCardName.visibility = View.GONE
-                if (movimentacao.isDespesaDinheiro) {
-                    holder.binding.imageMoney.visibility = View.VISIBLE
-                } else {
+                if (movimentacao.isDespesaPix) {
+                    holder.binding.txtCardName.visibility = View.GONE
+                    holder.binding.imagePix.visibility = View.VISIBLE
                     holder.binding.imageMoney.visibility = View.GONE
-                    if (movimentacao.isDespesaPix) {
-                        holder.binding.imagePix.visibility = View.VISIBLE
-                    } else {
-                        holder.binding.imageMoney.visibility = View.GONE
+                } else {
+                    if (movimentacao.isDespesaDinheiro) {
+                        holder.binding.txtCardName.visibility = View.GONE
+                        holder.binding.imagePix.visibility = View.GONE
+                        holder.binding.imageMoney.visibility = View.VISIBLE
                     }
                 }
             }
+
         } else {
             if (movimentacao.tipo == "r") {
                 holder.binding.textValor.setTextColor(context.resources.getColor(R.color.colorAccentReceita))
                 holder.binding.textValor.text = movimentacao.valor.toString()
 
                 if (movimentacao.isReceitaCartao) {
+                    holder.binding.txtCardName.text = movimentacao.cartaoCredito.nomeCartao
                     holder.binding.txtCardName.visibility = View.VISIBLE
-                    holder.binding.txtCardName.text = null
-                    holder.binding.imageMoney.visibility = View.GONE
                     holder.binding.imagePix.visibility = View.GONE
+                    holder.binding.imageMoney.visibility = View.GONE
+
                 } else {
-                    holder.binding.txtCardName.visibility = View.GONE
-                    if (movimentacao.isDespesaDinheiro) {
-                        holder.binding.imageMoney.visibility = View.VISIBLE
-                    } else {
+                    if (movimentacao.isReceitaPix) {
+                        holder.binding.txtCardName.visibility = View.GONE
+                        holder.binding.imagePix.visibility = View.VISIBLE
                         holder.binding.imageMoney.visibility = View.GONE
-                        if (movimentacao.isDespesaPix) {
-                            holder.binding.imagePix.visibility = View.VISIBLE
-                        } else {
-                            holder.binding.imageMoney.visibility = View.GONE
+                    } else {
+                        if (movimentacao.isReceitaDinheiro) {
+                            holder.binding.txtCardName.visibility = View.GONE
+                            holder.binding.imagePix.visibility = View.GONE
+                            holder.binding.imageMoney.visibility = View.VISIBLE
                         }
                     }
                 }
+
             }
         }
 
